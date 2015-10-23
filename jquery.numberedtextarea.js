fd255db6211a8ce09b79e576666c04afc5a3b154
Dariusz Arciszewski <darcisze@akamai.com>
2015-10-23 10:32:54 +0000

    allowTabChar

--- jquery.numberedtextarea.js
+++ jquery.numberedtextarea.js
@@ -15,10 +15,10 @@
     $.fn.numberedtextarea = function(options) {
  
         var settings = $.extend({
-            color:			null,        // Font color
-            borderColor:	null,    	 // Border color
-            class:			null,        // Add class to the 'numberedtextarea-wrapper'
-            allowTabChar:   false,		 // If true Tab key creates indentation
+            color:          null,        // Font color
+            borderColor:    null,        // Border color
+            class:          null,        // Add class to the 'numberedtextarea-wrapper'
+            allowTabChar:   false,       // If true Tab key creates indentation
         }, options);
  
         this.each(function() {
@@ -31,12 +31,26 @@
             addLineNumbers(this, settings);
             
             if(settings.allowTabChar) {
-            	$(this).allowTabChar();
+                $(this).allowTabChar();
             }
         });
         
         return this;
     };
+    
+    $.fn.allowTabChar = function() {
+        if (this.jquery) {
+            this.each(function() {
+                if (this.nodeType == 1) {
+                    var nodeName = this.nodeName.toLowerCase();
+                    if (nodeName == "textarea" || (nodeName == "input" && this.type == "text")) {
+                        allowTabChar(this);
+                    }
+                }
+            })
+        }
+        return this;
+    }
     
     function addWrapper(element, settings) {
         var wrapper = $('<div class="numberedtextarea-wrapper"></div>').insertAfter(element);
@@ -92,6 +106,37 @@
         element = $(element);
         var linesDiv = element.parent().find('.numberedtextarea-line-numbers');
         linesDiv.scrollTop(element.scrollTop());
+    }
+    
+    function pasteIntoInput(el, text) {
+        el.focus();
+        if (typeof el.selectionStart == "number") {
+            var val = el.value;
+            var selStart = el.selectionStart;
+            el.value = val.slice(0, selStart) + text + val.slice(el.selectionEnd);
+            el.selectionEnd = el.selectionStart = selStart + text.length;
+        } else if (typeof document.selection != "undefined") {
+            var textRange = document.selection.createRange();
+            textRange.text = text;
+            textRange.collapse(false);
+            textRange.select();
+        }
+    }
+
+    function allowTabChar(el) {
+        $(el).keydown(function(e) {
+            if (e.which == 9) {
+                pasteIntoInput(this, "\t");
+                return false;
+            }
+        });
+
+        // For Opera, which only allows suppression of keypress events, not keydown
+        $(el).keypress(function(e) {
+            if (e.which == 9) {
+                return false;
+            }
+        });
     }
  
 }(jQuery));
