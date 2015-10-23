98837ef01ac6a43591d982c2602574842ab06aa6
Dariusz Arciszewski <darcisze@akamai.com>
2015-10-23 08:37:59 +0000

    remove LICENSE, add initial files

---
+++ jquery.numberedtextarea.js
@@ -0,0 +1,25 @@
+/*
+ * NumberedTextarea - jQuery Plugin
+ * Textarea with line numbering
+ *
+ * Copyright (c) 2015 Dariusz Arciszewski
+ *
+ * Requires: jQuery v2.0+
+ *
+ * Licensed under the GPL licenses:
+ *   http://www.gnu.org/licenses/gpl.html
+ */
+
+(function ($) {
+ 
+    $.fn.numberedtextarea = function(options) {
+ 
+    	// Set options
+        var settings = $.extend({
+        }, options);
+ 
+        
+        return this;
+    };
+ 
+}(jQuery));
